7842c2d33dc35666fdaae06b3e0de8d0af20cce4
Ayush Gupta <7ayushgupta@gmail.com>
2019-06-08 17:50:32 +0530

    Removed recursive rep

--- LabVIEW/acoustic_processing/NI-Example-DLL/ni.py
+++
@@ -1,5 +0,0 @@
-print("Starting the program")
-
-import ctypes
-ctypes.cdll.LoadLibrary(r"./addLib.dll")
-print("Ending the program")
--- LabVIEW/acoustic_processing/headers/MUSIC.h
+++
@@ -1,9 +0,0 @@
-#include <Eigen/Dense>
-using namespace Eigen;
-#ifndef MUSIC_H
-#define MUSIC_H
-void neoEigen(Matrix2cf* , MatrixXcf*, VectorXcf*, int , int);
-void giveSteeringVector(float, Vector2cf*);
-void powerSp(Vector2cf, Matrix2cf*, int, int*, float);
-void MUSICclassifier(Matrix2cf *,  float *, float *);
-#endif
